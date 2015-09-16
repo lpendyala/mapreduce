6ac0ade4e645e8d5bab6c4056b89ecb04df656b2
Laks.Pendyala <laks.pendyala@gmail.com>
2015-09-16 15:20:19 -0500

    Project Commit

---
+++ mapreduce/src/main/java/laks/mapreduce/LaksApp.java
@@ -0,0 +1,13 @@
+package laks.mapreduce;
+
+/**
+ * Hello world!
+ *
+ */
+public class LaksApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
